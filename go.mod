8d79002a5a10284c0fdb3aaa8228f73e6640f803
leiwen <leiwen@heytea.com>
2021-07-26 18:18:07 +0800

    feat:新增gui包lorca启动前端

--- go.mod
+++ go.mod
@@ -10,4 +10,5 @@ require (
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/spf13/viper v1.7.1
+	github.com/zserge/lorca v0.1.10
 )
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"chatroom/initialize"
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/zserge/lorca"
+	"log"
 	"net/http"
 )
 
@@ -16,9 +18,22 @@ func main() {
 	router := mux.NewRouter()
 	//监听channel数据
 	go h.run()
+	//开启chrome
+	go StartChrome()
 	// 配置 websocket route
 	router.HandleFunc("/ws", myws)
 	if err := http.ListenAndServe("127.0.0.1:8080", router); err != nil {
 		fmt.Println("err:", err)
 	}
+}
+
+func StartChrome(){
+	// Create UI with basic HTML passed via data URI
+	ui, err := lorca.New("E:\\github\\websocket\\local.html", "", 480, 320)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ui.Close()
+	// Wait until UI window is closed
+	<-ui.Done()
 }
